c5c963e43ab88e612220ea17ad5655ceeda67e00
Carlos Yanez <cyanez@effenti.ca>
2023-04-13 18:13:26 -0400

    Feature/songFinder Add api giphy gif's for song title

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
-export {default as InputSongs} from "@components/InputSongs.component";
-export {default as CardSong} from  "@components/CardSongs.component";
+export { default as InputSongs } from "@components/InputSongs.component";
+export { default as CardSong } from "@components/CardSongs.component";
+export { default as GifSong } from "@components/GifSongs.component";
--- src/views/songs/TrackDetail.view.js
+++ src/views/songs/TrackDetail.view.js
@@ -10,8 +10,8 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import GetLiryc from "@api/ApiLyric.services";
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import { MSG_LYRIC_NOT_AVAILABLE } from "@utils/Constants.utils";
-import { CardSong } from '@components'
-import { MSG_SONG_DETAIL, MSG_TITLE_TRACK_LABEL } from "@utils/Constants.utils";
+import { CardSong, GifSong } from '@components'
+import { MSG_SONG_DETAIL, MSG_TITLE_TRACK_LABEL, MSG_GIF_SEARCH } from "@utils/Constants.utils";
 
 
 export default function TrackDetail() {
@@ -25,52 +25,65 @@ export default function TrackDetail() {
             setLyric(data)
         }
     })
+    let style = {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "space-between",
+        justifyContent: "center"
+    }
 
     return (
-        <>
-            <CardSong title={MSG_SONG_DETAIL} description="">
-                <div>
-                    <Card sx={{ display: 'flex' }}>
-                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                            <CardContent sx={{ flex: '1 0 auto' }}>
-                                <Typography component="div" variant="h5">
-                                    {data.title}
-                                </Typography>
-                                <Typography variant="subtitle1" color="text.secondary" component="div">
-                                    {data.artist}
-                                    <br />
-                                    {data.release_date}
-                                </Typography>
-                            </CardContent>
-                            <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-
-                                <IconButton aria-label="play/pause">
-                                    <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-                                </IconButton>
-
-                            </Box>
-                        </Box>
-                        <CardMedia
-                            component="img"
-                            sx={{ width: 151 }}
-                            image={data.image}
-                            alt={data.title}
-                        />
-
-                    </Card>
+        <div style={style}>
+            <div >
+                <CardSong title={MSG_GIF_SEARCH + data.title} description="">
+                    <GifSong song={data.title} />
+                </CardSong>
+            </div>
+            <div>
+                <CardSong title={MSG_SONG_DETAIL} description="">
                     <div>
-                        {MSG_TITLE_TRACK_LABEL}
-                        <br />
-                        <TextareaAutosize
-                            minRows={4}
-                            placeholder={MSG_LYRIC_NOT_AVAILABLE}
-                            readOnly
-                            defaultValue={lyric}
-                            style={{ width: 450 }}
-                        />
+                        <Card sx={{ display: 'flex' }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        {data.title}
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                                        {data.artist}
+                                        <br />
+                                        {data.release_date}
+                                    </Typography>
+                                </CardContent>
+                                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+
+                                    <IconButton aria-label="play/pause">
+                                        <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                                    </IconButton>
+
+                                </Box>
+                            </Box>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 151 }}
+                                image={data.image}
+                                alt={data.title}
+                            />
+
+                        </Card>
+                        <div>
+                            {MSG_TITLE_TRACK_LABEL}
+                            <br />
+                            <TextareaAutosize
+                                minRows={4}
+                                placeholder={MSG_LYRIC_NOT_AVAILABLE}
+                                readOnly
+                                defaultValue={lyric}
+                                style={{ width: 450 }}
+                            />
+                        </div>
                     </div>
-                </div>
-            </CardSong>
-        </>
+                </CardSong>
+            </div>
+        </div>
     )
 }
